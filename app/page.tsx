0e59d959f440d28a915a211680c8e583b70ddf0b
Otavio Lima <olima94@gmail.com>
2024-08-07 00:02:20 -0300

    feat: add header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,31 @@
+import { SearchIcon } from "lucide-react";
+import Header from "./_components/header";
 import { Button } from "./_components/ui/button";
+import { Input } from "./_components/ui/input";
+import Image from "next/image";
 
 export default function Home() {
-  return <Button>test</Button>;
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Otávio!</h2>
+        <p>Terça-feira 06 de Agosto</p>
+        <div className="mt-6 flex flex-row items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner-01.png"
+            alt="banner"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
