9d9344210740f5af483d79bb0f2bfe5d8ec9302a
Otavio Lima <olima94@gmail.com>
2024-08-09 23:28:49 -0300

    chore: add next auth setup

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -2,15 +2,13 @@ import { Card, CardContent } from "./ui/card";
 
 const Footer = () => {
   return (
-    <footer>
-      <Card>
-        <CardContent className="px-5 py-6">
-          <p className="text-sm text-gray-400">
-            © 2023 Copyright <span className="font-bold">Barber Time</span>
-          </p>
-        </CardContent>
-      </Card>
-    </footer>
+    <Card>
+      <CardContent className="px-5 py-6">
+        <p className="text-sm text-gray-400">
+          © 2023 Copyright <span className="font-bold">Barber Time</span>
+        </p>
+      </CardContent>
+    </Card>
   );
 };
 
--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -1,10 +1,17 @@
-import { HomeIcon, CalendarIcon, LogOutIcon } from "lucide-react";
+import { HomeIcon, CalendarIcon, LogOutIcon, LogInIcon } from "lucide-react";
 import { quickSearchOptions } from "../_constants/search";
 import { Button } from "./ui/button";
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet";
-import { Avatar, AvatarImage } from "./ui/avatar";
 import Image from "next/image";
 import Link from "next/link";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
 
 const SidebarSheet = () => {
   return (
@@ -12,14 +19,35 @@ const SidebarSheet = () => {
       <SheetHeader>
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
-      <div className="flex items-center gap-3 border-b border-solid py-5">
-        <Avatar>
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+        <h2 className="font-bold">Olá, faça o seu login</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do Google.
+              </DialogDescription>
+            </DialogHeader>
+            <Button variant="outline" className="gap-1 font-bold">
+              <Image src="/google.svg" alt="google" width={18} height={18} />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+
+        {/* <Avatar>
           <AvatarImage src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=2980&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
         </Avatar>
         <div>
           <p className="font-bold">Otávio Lima</p>
           <p className="text-xs">olima94@gmail.com</p>
-        </div>
+        </div> */}
       </div>
       <div className="flex flex-col gap-2 border-b border-solid py-5">
         <SheetClose asChild>
